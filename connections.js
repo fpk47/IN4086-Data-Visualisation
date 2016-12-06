18bc08fc145087d8166fc4c3fcabf27fbd6204cf
Wouter <wa.groen@gmail.com>
2016-12-06 12:36:12 +0100

    Fixed railway connections

--- connections.js
+++ connections.js
@@ -2,37 +2,49 @@
  * 
  */
 
+function getMax(columns, data) {
+	var res = [];
+	for (var i_columns = 0; i_columns < columns.length; i_columns++) {
+		res[i_columns] = Number.MIN_VALUE;
+	}
+	data.forEach(function(d) {
+		for (var i_columns = 0; i_columns < columns.length; i_columns++) {
+			res[i_columns] = Math.max(res[i_columns], d[columns[i_columns]]);
+		}
+	});
+	return res;
+}
+
+function getMin(columns, data) {
+	var res = [];
+	for (var i_columns = 0; i_columns < columns.length; i_columns++) {
+		res[i_columns] = Number.MAX_VALUE;
+	}
+	data.forEach(function(d) {
+		for (var i_columns = 0; i_columns < columns.length; i_columns++) {
+			res[i_columns] = Math.min(res[i_columns], d[columns[i_columns]]);
+		}
+	});
+	return res;
+}
+
+
 function join(lookupTable, mainTable, lookupKey, mainKey, select) {
-	//alert("join");
-	var n = 0;
     var l = lookupTable.length,
         m = mainTable.length,
         lookupIndex = new Map(),
-        //lookupIndex = [],
         output = [];
-    //alert(l + ", " + m + ", ");
     for (var i = 0; i < l; i++) { // loop through l items
         var row = lookupTable[i];
-        //lookupIndex[row[lookupKey]] = row; // create an index for lookup table
-        lookupIndex.set(row[lookupKey], row);
-        //console.log(lookupIndex);
-        if (n < -5) {
-        	n++;
-        	console.log(row);
-        	//alert(row.code)
-        	console.log(lookupKey);
-        	console.log(row[lookupKey]);
-        	console.log(lookupIndex);
-        }
+        lookupIndex.set(row[lookupKey], row); // create an index for lookup table
     }
     for (var j = 0; j < m; j++) { // loop through m items
         var y = mainTable[j];
         var x = lookupIndex.get(y[mainKey]); // get corresponding row from lookupTable
-        //var x = lookupIndex[y[mainKey]];
         output.push(select(y, x)); // select only the columns you need
     }
     return output;
-};
+}
 
 var conns = d3.csv("http://localhost/IN4086Link/IN4086-Data-Visualisation/data/connections0.csv", function(dataConns) {
 	stats = d3.csv("http://localhost/IN4086Link/IN4086-Data-Visualisation/data/stations-nl-2016-02.csv", function(dataStats) {
@@ -67,60 +79,69 @@ function getJoinAndRender(stations, connections) {
 	    };
 	});
 	
+	var resultFullClean = [];
+	resultFull.forEach(function(d) {
+		if (d.s1_lat != null && d.s1_lng != null && d.s2_lat != null && d.s2_lng != null) {
+			resultFullClean.push(d);
+		}
+	});
+	
 	var radius = 2;
-	var size = 1100 - 2*radius;
+	var height = 900;
 
 	var maxX = 0;
 	var minX = 99999999;
 	var maxY = 0;
 	var minY = 99999999;
-	for (i_data = 0; i_data < resultFull.length; i_data++) {
-	    var z = resultFull[i_data];
+	resultFullClean.forEach(function(d) {
+	    var z = d;
 	    maxX = Math.max(maxX, z.s1_lng);
 	    maxX = Math.max(maxX, z.s2_lng);
-	    /*if (!(z.s1 > 0)) {
-	    	console.log(z);
-	    }*/
-	    //console.log(z);
 	    minX = Math.min(minX, z.s1_lng);
 	    minX = Math.min(minX, z.s2_lng);
 	    maxY = Math.max(maxY, z.s1_lat);
 	    maxY = Math.max(maxY, z.s2_lat);
 	    minY = Math.min(minY, z.s1_lat);
 	    minY = Math.min(minY, z.s2_lat);
-	}
+	});
 
 	var diff = (maxY - minY) / (maxX - minX);
-	var xWidth = diff * size;
-	var xOffset = (size - xWidth) / 2;
+	//var diff = (maxX - minX) / (maxY - minY);
+	var width = diff * height;
+
+	var svgContainer = d3.select("#cContainer").append("svg")
+										.attr("width", width)
+										.attr("height", height)
+										.attr("id", "vis");
+										
+	var lines = svgContainer.selectAll("line")
+					.data(resultFullClean);
 	
-	//alert(diff + ", " + xWidth + ", " + xOffset + ", " + maxX + ", " + minX + ", " + maxY + ", " + minY);
+	var linearScaleX = d3.scaleLinear()
+						.domain([minX,maxX])
+						.range([radius,width-radius]);
+	var linearScaleY = d3.scaleLinear()
+						.domain([minY,maxY])
+						.range([height-radius,radius]);
 	
-	var vis = d3.select("#vis");
-	
-	var lines = vis.selectAll("line")
-					.data(resultFull);
-					
 	lines.enter()
 	    .append("line")
-	    .attr("x1", function(d){ return d.s1_lng*100;})
-		.attr("y1", function(d){ return d.s1_lat*100;})
-		.attr("x2", function(d){ return d.s2_lng*100;})
-		.attr("y2", function(d){ return d.s2_lat*100;})
-	   /*.attr("x1", function(d){ return (d.s1_lng-minX)*100;})
-		.attr("y1", function(d){ return (d.s1_lat-minY)*100;})
-		.attr("x2", function(d){ return (d.s2_lng-minX)*100;})
-		.attr("y2", function(d){ return (d.s2_lat-minY)*100;})*/
-	    /*.attr("x1", function(d){return getX(d, minX, maxX, xWidth, xOffset, radius);})
-	    .attr("y1", function(d){return (1- ((d.s1_lat - minY) / (maxY - minY))) * size + radius;})
-	    .attr("x1", function(d){return (d.s1_lng - minX) / (maxX - minX) * xWidth + radius + xOffset;})
-		.attr("y2", function(d){return (1- ((d.s1_lat - minY) / (maxY - minY))) * size + radius;})*/
+	    .attr("x1", function(d){ return linearScaleX(d.s1_lng);})
+		.attr("y1", function(d){ return linearScaleY(d.s1_lat);})
+		.attr("x2", function(d){ return linearScaleX(d.s2_lng);})
+		.attr("y2", function(d){ return linearScaleY(d.s2_lat);})
 		.attr("stroke-width", 2)
 		.attr("stroke", "black");
-}
-
-function getX(d, minX, maxX, xWidth, xOffset, radius) {
-	var res = (d.s1_lng - minX) / (maxX - minX) * xWidth + radius + xOffset;
-	console.log(res);
-	return res;
+	
+	var circles =  svgContainer.selectAll("circle").
+						data(stations);
+	
+	circles.enter()
+		.append("circle")
+		.style("fill", "red")
+	    .attr("cx", function(d){return linearScaleX(d.geo_lng);})
+	    .attr("cy", function(d){return linearScaleY(d.geo_lat);})
+	    .attr("r",radius);
+	
+	
 }
